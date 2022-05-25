317711c88ae9158e9a17b3b7371f702648da2c30
Julio <jcollazo@users.noreply.github.com>
2022-05-25 15:57:18 +0000

    master

--- src/constants.ts
+++ src/constants.ts
@@ -1,1 +1,9 @@
 
+export const ACCEPTABLE_ENTER_RANGE = ;
+export const CONFIRMATION_TIMES = ;
+export const LOSS_TO_PROFIT_RATIO = , 
+export const NEW_HIGH_TARGET_PROFIT_FROM_ENTRY_RATIO = , 
+export const SECONDS_CHECK_BUY = , 
+export const SECONDS_CHECK_SELL =, 
+export const SECONDS_UPDATE_QUOTES =, 
+export const MINUTES_UPDATE_LEVELS = 1;
