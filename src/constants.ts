1934a957f261fc22fd96f1077adb0a4e114136f7
Julio <jcollazo@users.noreply.github.com>
2022-05-25 17:33:49 +0000

    master

--- src/constants.ts
+++ src/constants.ts
@@ -1,9 +1,18 @@
 
-export const ACCEPTABLE_ENTER_RANGE = ;
-export const CONFIRMATION_TIMES = ;
-export const LOSS_TO_PROFIT_RATIO = , 
-export const NEW_HIGH_TARGET_PROFIT_FROM_ENTRY_RATIO = , 
-export const SECONDS_CHECK_BUY = , 
-export const SECONDS_CHECK_SELL =, 
-export const SECONDS_UPDATE_QUOTES =, 
+export const ACCEPTABLE_ENTER_RANGE = 2;
+export const CONFIRMATION_TIMES = 2;
+export const LOSS_TO_PROFIT_RATIO = 8.5; 
+export const NEW_HIGH_TARGET_PROFIT_FROM_ENTRY_RATIO = 10.0;
+export const SECONDS_CHECK_BUY = 5; 
+export const SECONDS_CHECK_SELL = 5; 
+export const SECONDS_UPDATE_QUOTES = 5; 
 export const MINUTES_UPDATE_LEVELS = 1;
+export const AVAILABLE_CASH = 10000;
+export const MINUTES_DETERMINE_DIRECTION=15;
+export const SIDEWAY_GRADIENT_MARGIN=2;
+export const DEFAULT_TICKER='SPY';
+export const WATCHLIST_PATH='./watchlist.txt';
+export const DEFAULT_RESOLUTION='Buy';
+export const FINNHUB_TOKEN="ca6049aad3ib7i7rsic0";
+export const TDA_API_KEY="7BZOJIJVRSS9GLI8WXGAGSHN1XC2OMJQ";
+export const COMMUNICATION_PORT=80;
